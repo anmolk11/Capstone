b764f5607d2196a98e46d9f592b3a8abffa6083c
anmolk11 <akcp.cpp@gmail.com>
2023-12-07 00:19:51 +0530

    added all the data to sql

--- Testing/main.py
+++ Testing/main.py
@@ -12,7 +12,27 @@ db_params = {
     'password': 'anmol147',
 }
 
-def insert(data_to_insert):
+def fix_na(data):
+   cleaned_tuple = tuple(None if pd.isna(value) else value for value in data)
+   return cleaned_tuple
+
+def insert(query,data):
+    try:
+        connection = psycopg2.connect(**db_params)
+        cursor = connection.cursor()
+        cursor.execute(query, data)
+        connection.commit()
+        # print("Data inserted successfully!")
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error:", error)
+
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+
+def insert_hr(data_to_insert):
     insert_query = """
     INSERT INTO hr_core (
         "Employee_Name", "Employee Number", "State", "Zip", "DOB", "Age", "Sex",
@@ -26,29 +46,46 @@ def insert(data_to_insert):
         %s, %s, %s, %s, %s, %s, %s
     );
     """
-    try:
-        connection = psycopg2.connect(**db_params)
-        cursor = connection.cursor()
-        cursor.execute(insert_query, data_to_insert)
-        connection.commit()
-        # print("Data inserted successfully!")
+    insert(insert_query,data_to_insert)
 
-    except (Exception, psycopg2.Error) as error:
-        print("Error:", error)
+def insert_energy(data_to_insert):
+    insert_query = """
+    INSERT INTO energy_consumption (
+        date, Usage_kWh, Lagging_Current_Reactive_Power_kVarh, 
+        Leading_Current_Reactive_Power_kVarh, CO2_tCO2, 
+        Lagging_Current_Power_Factor, Leading_Current_Power_Factor, 
+        NSM, WeekStatus, Day_of_week, Load_Type,time
+    ) VALUES (
+        %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
+    );
+    """
+    insert(insert_query,data_to_insert)
 
-    finally:
-        if connection:
-            cursor.close()
-            connection.close()
+def insert_supply_chain(data_to_insert):
+    insert_query = """
+    INSERT INTO supply_chain (
+    "Product type", SKU, Price, Availability, "Number of products sold",
+    "Revenue generated", "Customer demographics", "Stock levels", "Lead times",
+    "Order quantities", "Shipping times", "Shipping carriers", "Shipping costs",
+    "Supplier name", Location, "Lead time", "Production volumes",
+    "Manufacturing lead time", "Manufacturing costs", "Inspection results","Defect rates","Transportation modes",
+    "Routes","Costs"
+    ) VALUES (
+    %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
+    );
+    """
+    insert(insert_query,data_to_insert)
 
-def fix_na(data):
-   cleaned_tuple = tuple(None if pd.isna(value) else value for value in data)
-   return cleaned_tuple
 
-
-if __name__ == '__main__':
-    df = pd.read_csv('../datasets/HR-Data/core_dataset.csv')
+def insert_df(path,func):
+    df = pd.read_csv(path)
+    # df['date'] = pd.to_datetime(df['date'], format = '%d/%m/%Y')
     for index, data in tqdm(df.iterrows(), total=len(df), desc="Inserting", unit="row"):
         data = tuple(data)
-        insert(fix_na(data))
-        
+        func(fix_na(data))
+
+if __name__ == '__main__':
+    # hr_path = '../datasets/HR-Data/core_dataset.csv'
+    # energy_path = '../datasets/Energy Consumption/energy.csv'
+    supply_chain_path = '../datasets/Supply Chain/supply_chain.csv'
+    insert_df(supply_chain_path,insert_supply_chain)
